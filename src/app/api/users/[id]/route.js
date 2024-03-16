acf1d51991ee5489bb1f81e1bcba536a7f3f10ec
ronalgithub <surel.ronal@gmail.com>
2024-03-16 07:52:58 +0000

    buat crud api user

---
+++ src/app/api/users/[id]/route.js
@@ -0,0 +1,51 @@
+import { prisma } from "@/utils/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(_, {params}) {
+   
+    const userId = params.id;
+
+    //business logic
+    const userData = await prisma.user.findFirst ({
+        where: {
+            id: userId
+        }
+    })
+
+    //handling response
+    return NextResponse.json ({mesagge:"get user by id success", data:userData})
+
+}
+
+
+export async function PATCH (req, {params}) {
+    const userId = params.id;
+    const {nama, password, role, email, phone } = await req.json()
+
+    const updatedData = await prisma.user.update ({
+        where: {
+            id:userId,
+        },
+        data: {
+            nama: nama,
+            password: password,
+            role: role,
+            email,
+            phone
+        }
+    })
+
+    return NextResponse.json ({message: "Update user Success", data: updatedData})
+}
+
+export async function DELETE (_, {params}) {
+    const userId = params.id;
+
+        await prisma.user.delete({        
+        where: {
+            id: userId,
+        },
+    })
+
+    return NextResponse.json ({mesagge:"Delete user success"});
+}
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,7 +1,29 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
-import prisma from "@/utils/prisma";
+import prisma from "../../../../prisma/client";
+
+
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams;
+  const role = searchParams.get("role");
+
+  if (role )  {
+      const allUser = await prisma.user.findMany({
+          where: {
+                    role: {
+                      contains: role,                        
+                  },
+            }
+      });
+
+      return NextResponse.json ({mesage: "get all users by role  success", data: allUser})
+  }  
+
+  const allUser = await prisma.user.findMany();
+  return NextResponse.json({message: "get all user success", data: allUser})
+}
+
 
 export async function POST(req) {
   const { nama, email, username, password, phone, role } = await req.json();
@@ -33,3 +55,5 @@ export async function POST(req) {
     );
   }
 }
+
+
